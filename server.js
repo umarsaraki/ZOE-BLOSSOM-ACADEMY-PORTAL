c1b88d4a7bbce4dff3dc7cbc1f0d0c10a57130e9
umarsaraki <umarsaraki81@gmail.com>
2026-08-27 09:45:02 +0100

    Update PORT variable to include process.env.PORT

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const SQLiteStore = require('connect-sqlite3')(session);
 const db = require('./db');
 
 const app = express();
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || process.env.APP_PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
